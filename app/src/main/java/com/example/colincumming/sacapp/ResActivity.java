6503b3f6b804491903af0495b30dadb2f5d1b7d5
16cmc21@queensu.ca <16cmc21@queensu.ca>
2018-03-07 12:56:01 -0500

    Commit with family branch done, needs xml layout work

---
+++ app/src/main/java/com/example/colincumming/sacapp/ResActivity.java
@@ -0,0 +1,19 @@
+package com.example.colincumming.sacapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ResActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_res);
+    }
+    public void startHome(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
